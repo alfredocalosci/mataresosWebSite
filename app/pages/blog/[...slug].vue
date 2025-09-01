95dffab01e9461359dd417372eff8c022db40903
alfredocalosci <alfredoc@negot.net>
2025-09-01 18:40:07 +0200

    firstCommit

---
+++ app/pages/blog/[...slug].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection('blog').path(route.path).first()
+})
+</script>
+
+<template>
+  <h1 class="text-4xl font-bold" v-if="page">{{ page.title }}</h1>
+  <ContentRenderer v-if="page" :value="page" />
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-bold">Hello World</h1>
+    <p>a short intro ...</p>
+
+    <hr/>
+
+     <!-- link to blog -->
+   <NuxtLink to="/blog">Blog</NuxtLink>
+  </div>
+
+ 
+</template>
